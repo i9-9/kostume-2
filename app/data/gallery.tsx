5a4288ac1c7591ccb9e7c21a12823442248b0346
1TB <florencgmz@gmail.com>
2024-08-04 18:59:34 -0300

    deleting files

--- app/data/gallery.tsx
+++ app/data/gallery.tsx
@@ -8,7 +8,7 @@ const images = [
     { src: '/WOMEN.jpg', title: 'KOSTÜME WOMEN', link: "women/", type: "image" },
     { src: '/SEAMM.mp4', title: 'SEAMM', link: "https://seamm.io/marketplace?search=kost%C3%BCme", type: "video" },
     { src: '/MEN.jpg', title: 'KOSTÜME MEN', link: "men/", type: "image"},
-    { src: '/x3-mujer.mp4', title: '#47AW24', link: "47aw24/", type: "video"  },
+    { src: '/47.mp4', title: '#47AW24', link: "47aw24/", type: "video"  },
     { src: '/STORE.jpg', title: 'KOSTÜME STORE', link: "https://g.co/kgs/uT96wdh ", type: "image" },
   ];
 
