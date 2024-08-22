c7030f52ea70034b20206211cb5918c9cc0bb57f
i9-9 <yvan.vrs@gmail.com>
2024-08-22 15:13:38 -0300

    links fix

--- app/data/es-menu.tsx
+++ app/data/es-menu.tsx
@@ -1,6 +1,6 @@
 const menuItemsEs = [
     { href: "47aw24", label: "#47AW24", subcategories: [], links: [] },
-    { href: "48SS25", label: "#48SS25", subcategories: [], links: [] },
+    { href: "48ss25", label: "#48SS25", subcategories: [], links: [] },
     { href: "men", label: "MEN", subcategories: ["REMERAS", "CAMISAS", "FALDAS Y PANTALONES", "ABRIGOS", "ENTEROS", "ACCESORIOS"], links: ["remeras", "camisas1", "faldas-y-pantalones1", "abrigos1", "enteros", "accesorios1"] },
     { href: "women", label: "WOMEN", subcategories: ["TOPS Y REMERAS", "CAMISAS", "VESTIDOS Y ENTEROS", "FALDAS Y PANTALONES", "ABRIGOS", "BODIES Y TRAJES DE BAÑO", "ACCESORIOS"], links: ["tops-y-remeras", "camisas", "vestidos-y-enteros", "faldas-y-pantalones", "abrigos", "bodies-y-trajes-de-bano", "accesorios"] },
     { href: "wild-object", label: "WILD OBJECT", subcategories: [], links: [] },
