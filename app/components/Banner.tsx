55dae1434aced56d7e4bea8e0f480b049c44d112
i9-9 <yvan.vrs@gmail.com>
2024-10-30 15:01:28 -0300

    responsive banner now handled with Tailwind instead of JS

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,31 +1,26 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import banner from '../data/banner';
 
 const Banner: React.FC = () => {
-  const [isMobile, setIsMobile] = useState<boolean>(typeof window !== 'undefined' && window.innerWidth < 768);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  const images = isMobile ? banner.mobile : banner.desktop;
-
   return (
-    <div className="px-4 lg:px-8 my-4">     
-      {images.map((image, index) => (
-        <a key={index} href={image.link} target="_blank" rel="noopener noreferrer">
-          <img src={image.src} alt={image.title} className="w-full h-auto" />
-        </a>
-      ))}
+    <div className="px-4 lg:px-8 my-4">
+      <div className="block lg:hidden">
+        {banner.mobile.map((image, index) => (
+          <a key={index} href={image.link} target="_blank" rel="noopener noreferrer">
+            <img src={image.src} alt={image.title} className="w-full h-auto" />
+          </a>
+        ))}
+      </div>
+
+      <div className="hidden lg:block">
+        {banner.desktop.map((image, index) => (
+          <a key={index} href={image.link} target="_blank" rel="noopener noreferrer">
+            <img src={image.src} alt={image.title} className="w-full h-auto" />
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
