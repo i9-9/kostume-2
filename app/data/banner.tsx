ffcdf6316eef896f0af6f5d5bb6315326a028882
i9-9 <yvan.vrs@gmail.com>
2024-10-31 21:00:40 -0300

    blue monday refresh complete

--- app/data/banner.tsx
+++ app/data/banner.tsx
@@ -7,8 +7,8 @@ type: "image" | "video";
 interface ImageSets { desktop: ImageProps[]; mobile: ImageProps[]; }
 
 const banner: ImageSets = {
-  desktop: [{ src: '/blue-monday/blue-monday-dt.jpg', title: 'Blue Monday', link: 'https://your-link.com', type: 'image' }],
-  mobile: [{ src: '/blue-monday/blue-monday-m.jpg', title: 'Your Banner Title', link: 'https://your-link.com', type: 'image' }],
+  desktop: [{ src: '/blue-monday/blue-monday-dt.jpg', title: 'Blue Monday', link: 'https://eshop.kostumeweb.net/ar/blue-monday', type: 'image' }],
+  mobile: [{ src: '/blue-monday/blue-monday-m.jpg', title: 'Your Banner Title', link: 'https://eshop.kostumeweb.net/ar/blue-monday', type: 'image' }],
 };
 
 export default banner;
